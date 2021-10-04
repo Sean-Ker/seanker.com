e5b4b56b59022ad7a80b6fd84a0598eaa20f5e1a
Sean Kernitsman <sean.kermail@gmail.com>
2021-10-04 15:17:06 -0400

    Finished hero section

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,12 @@
 $(function () {
     console.log("Init js.");
     $(window).scrollTop(0);
+
+    $("nav .nav-link").on("click", function (e) {
+        // e.preventDefault();
+        $(this).parents().find(".active").removeClass("active");
+        $(this).addClass("active");
+    });
 });
 
 $(window).scroll(function (event) {
