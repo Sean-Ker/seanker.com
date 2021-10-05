ea779a4cb25d0977c903089f1be41d4f7879087b
Sean Kernitsman <sean.kermail@gmail.com>
2021-10-04 20:41:59 -0400

    commit

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,33 @@
 $(function () {
     console.log("Init js.");
-    $(window).scrollTop(0);
 
     $("nav .nav-link").on("click", function (e) {
         // e.preventDefault();
         $(this).parents().find(".active").removeClass("active");
         $(this).addClass("active");
     });
+
+    // var scene = document.getElementById("scene");
+    // var parallaxInstance = new Parallax(scene);
+
+    // const img = document.getElementById("skills_hive");
+    // console.log(img);
+    // new simpleParallax(img, {
+    //     orientation: "right",
+    // });
+
+    console.log("here");
+    $(".js-tilt").tilt({
+        scale: 1.2,
+        glare: true,
+        maxGlare: 0.5,
+        // axis: "x",
+    });
+
+    var thehash = e.target.hash;
+    $(thehash).prop("id", thehash.substr(1) + "-noscroll");
+    window.location.hash = e.target.hash;
+    $(thehash + "-noscroll").prop("id", thehash.substr(1));
 });
 
 $(window).scroll(function (event) {
